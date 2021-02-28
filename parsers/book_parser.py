5200edab0a11dac7dd38404c992cfc4f1711d925
Gurupra5ad <guruprasad2511@gmail.com>
2021-02-28 16:14:09 +0530

    Finished book scrapping

--- parsers/book_parser.py
+++ parsers/book_parser.py
@@ -1,47 +1,72 @@
 import re
-from locators.book_locators import BookLocators 
+import logging
+
+from locators.book_locators import BookLocators
+
+logger = logging.getLogger('scraping.book_parser')
+
 
 class BookParser:
+    """
+    A class to take in an HTML page or content, and find properties of an item
+    in it.
+    """
 
     RATINGS = {
-        'one':1,
-        'Two':2,
-        'Three':3
-        'Four':4,
-        'Five':5
+        'One': 1,
+        'Two': 2,
+        'Three': 3,
+        'Four': 4,
+        'Five': 5
     }
 
     def __init__(self, parent):
-        self.soup = parent
+        logger.debug(f'New book parser created from `{parent}`')
+        self.parent = parent
+
+    def __repr__(self):
+        return f'<Book {self.name} {self.price}, {self.rating} stars>'
 
     @property
     def name(self):
+        logger.debug('Finding book name...')
         locator = BookLocators.NAME_LOCATOR
-        item.link = self.parent.select_one(locator)
-        item_name = item.link.attrs['title']
+        item_name = self.parent.select_one(locator).attrs['title']
+        logger.info(f'Found book name, `{item_name}`.')
         return item_name
 
     @property
     def link(self):
+        logger.debug('Finding book page link...')
         locator = BookLocators.LINK_LOCATOR
-        item_link = self.parent.select_one(locator).attrs['href']
-        return item_link
+        item_url = self.parent.select_one(locator).attrs['href']
+        logger.info(f'Found book page link, `{item_url}`.')
+        return item_url
 
     @property
     def price(self):
+        logger.debug('Finding book price...')
         locator = BookLocators.PRICE_LOCATOR
         item_price = self.parent.select_one(locator).string
-        pattern = '€([0-9]+\.[0-9]+)'
-        matcher = re.serch(pattern, item_price)  
-        return float(matcher.group(1))
+        logger.debug(f'Item price element found, `{item_price}`')
+
+        pattern = '£([0-9]+\.[0-9]+)'
+        matcher = re.search(pattern, item_price)
+        price = float(matcher.group(1))
+        logger.info(f'Found book price, `{price}`.')
+        return price
 
     @property
     def rating(self):
+        logger.debug('Finding book rating...')
         locator = BookLocators.RATING_LOCATOR
-        star_rating_tag = self.parent.select_one(locator)
-        classes = star_rating_tag.attrs['class']
-        rating_classes = [r for r in classes if r != 'star-rating']
-        rating_number = BookParser.RATINGS.get(rating_classes[0])
-        return rating_number
+        star_rating_element = self.parent.select_one(locator)
+        classes = star_rating_element.attrs['class']
+        rating_classes = filter(lambda x: x != 'star-rating', classes)
+        rating_class = next(rating_classes)
 
-
+        logger.debug(f'Found rating class, `{rating_class}`.')
+        logger.debug('Converting to integer for sorting.')
+        rating = BookParser.RATINGS.get(rating_class)
+        logger.info(f'Found book rating, `{rating}`.')
+        return rating
